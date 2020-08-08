154d5fd05d936bb62b852d880e972bde463f8a0f
Eric Ampire <ericampire.top@gmail.com>
2020-08-08 13:38:29 +0200

    Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,26 @@
 plugins {
     kotlin("jvm") version "1.3.72"
+    application
 }
 
-group = "org.example"
+application {
+    mainClassName = "com.ericampire.web.MainKt.main"
+}
+
+group = "org.ericampire.web"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
-    val ktor_version = ""
+    val ktor_version = "1.3.2"
     implementation(kotlin("stdlib"))
+
     implementation("io.ktor:ktor-thymeleaf:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+    implementation("ch.qos.logback:logback-classic:1.2.3")
 }
